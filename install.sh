ac47fbdfd0bcd02e004bcaf50a7caa2fbc4f20f2
pizeroLOL <71167373+pizeroLOL@users.noreply.github.com>
2022-04-26 21:27:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
-Get_Dist_Name()
-{
-    if grep -Eqii "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
-        DISTRO='CentOS'
-        PM='yum'
-    elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
-        DISTRO='RHEL'
-        PM='yum'
-    elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
-        DISTRO='Aliyun'
-        PM='yum'
-    elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
-        DISTRO='Fedora'
-        PM='yum'
-    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
-        DISTRO='Debian'
-        PM='apt'
-    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
-        DISTRO='Ubuntu'
-        PM='apt'
-    elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
-        DISTRO='Raspbian'
-        PM='apt'
-    else
-        DISTRO='unknow'
-        PM='pacman'
-    fi
-    export StPAutoInstallModel=$PM;
-}
-Get_Dist_Name
+if grep -Eqii "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+    sudo yum update git wget -y
+    sudo yum install java-1.8.0-openjdk git wget -y
+elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
+    sudo yum update git wget -y
+    sudo yum install java-1.8.0-openjdk git wget -y
+elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
+    sudo yum update -y
+    sudo yum install java-1.8.0-openjdk git wget -y
+elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+    sudo yum update -y
+    sudo yum install java-1.8.0-openjdk git wget -y
+elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+    sudo apt update
+    sudo apt install openjdk-8-jdk git wget -y
+elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+    sudo apt update
+    sudo apt install openjdk-8-jdk git wget -y
+elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+    sudo apt update
+    sudo apt install openjdk-8-jdk git wget -y'
+else
+    echo 请自行下载openjdk8
+fi
+
+sh dl-stp-server.sh
